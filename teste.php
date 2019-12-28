4d2aa35454b0a057085217ef0f8e0a73b0267aba
Diego Sampaio <diego.estaleiro@gmail.com>
2019-12-28 11:02:08 -0300

    Arquivo teste de conexão

---
+++ teste.php
@@ -0,0 +1,10 @@
+<?php
+$host = 'localhost';
+$user = 'root';
+$pass = '132465';
+$bd   = 'bd_teste';
+$conexao = new mysqli($host, $user, $pass, $bd);
+// verifica se houve erro na conexão
+if(!$conexao){
+    echo "ERROR: " . $conexao->error;
+}
